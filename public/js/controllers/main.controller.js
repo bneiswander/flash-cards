18e8cad5ed850f8814f14b30c84956c1c0086b5a
Ben Neiswander <ben.neiswander@gmail.com>
2015-05-27 15:01:29 -0400

    change the loader and the category selector

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -1,22 +1,29 @@
 app.controller('MainController', function($scope, FlashCardsFactory, ScoreFactory) {
 	$scope.flashCards;
 	$scope.selectedCategory = 'all';
-	$scope.categories = ['MongoDB','Express','Angular','Node']
+	$scope.categories = ['MongoDB','Express','Angular','Node', 'View All']
 	$scope.isLoading = true
+	$scope.progress = 0;
 
 	$scope.getCategoryCards = function(category){
 		$scope.isLoading = true;
+		$scope.progress = 100;  
+		if(category === 'View All'){
+			category = null;
+		}
 		FlashCardsFactory.getFlashCards(category).then(function(flashCards){
 			$scope.selectedCategory = category;
 			$scope.flashCards = flashCards;
 			ScoreFactory.reset();
 			$scope.isLoading = false;
+			$scope.progress = 0;
 		});
 	}
 
-	FlashCardsFactory.getFlashCards().then(function(flashCards) {
-		$scope.flashCards = flashCards;
-		$scope.isLoading = false
+	FlashCardsFactory.getFlashCards()
+		.then(function(flashCards) {
+			$scope.flashCards = flashCards;
+			$scope.isLoading = false
 
 	});
 });
